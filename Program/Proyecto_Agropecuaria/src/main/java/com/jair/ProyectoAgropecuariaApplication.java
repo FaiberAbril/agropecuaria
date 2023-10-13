4fffcc418d3ce87e6f96f348c5d359d20c7e1a18
Edinson Avellaneda <avellanedaedinson74@mail.com>
2023-10-13 09:34:28 -0500

    Creacion del Proyecto en Spring

---
+++ Program/Proyecto_Agropecuaria/src/main/java/com/jair/ProyectoAgropecuariaApplication.java
@@ -0,0 +1,13 @@
+package com.jair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAgropecuariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAgropecuariaApplication.class, args);
+	}
+
+}
