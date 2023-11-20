d96ad6929470820772e0b3875e4f11d20737faca
Edinson Avellaneda <avellanedaedinson74@mail.com>
2023-11-20 11:54:47 -0500

    Creacion Vista Pedido

--- Program/Proyecto_Agropecuaria/src/main/java/com/jair/controladores/PedidoControlador.java
+++ Program/Proyecto_Agropecuaria/src/main/java/com/jair/controladores/PedidoControlador.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.jair.modelos.Detalle;
 import com.jair.modelos.Pedido;
+import com.jair.modelos.Producto;
 import com.jair.servicios.PedidoServicios;
 import com.jair.servicios.ProductoServicios;
+
+import jakarta.websocket.server.PathParam;
 
 
 @Controller
@@ -23,12 +28,13 @@ public class PedidoControlador {
 	private ProductoServicios productoServicios;
 	
 	@GetMapping("/formPedido/{IdProducto}")
-	public String formPedido(Model model){
-		
+	public String formPedido(Model model, @PathVariable("IdProducto") long producto){
 		model.addAttribute("ObjPedido", new Pedido());
 		model.addAttribute("ObjDetalle", new Detalle());
+		Producto p = productoServicios.BuscarProducto(producto);
+		model.addAttribute("ObjProducto", p);
 		
-		return "";
+		return "formPedido";
 	}
 	
 }
